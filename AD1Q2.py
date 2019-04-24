470f18bd7bfd8b25e16636d25606d53e41e4da1b
renatocarusouff <49883782+renatocarusouff@users.noreply.github.com>
2019-04-24 19:39:49 -0300

    ..
    ..

---
+++ AD1Q2.py
@@ -0,0 +1,16 @@
+lista = list(map(float, input().split()))
+somatorio = 0
+listaAcimaDaMedia = []
+
+for i in lista:
+    somatorio += i
+media = somatorio / len(lista)
+for i in lista:
+    if i > media: listaAcimaDaMedia.append(i)
+
+print("Media = %.1f" % media)
+print("Listagem dos numeros acima da media")
+for i in listaAcimaDaMedia:
+    print("%.1f" % i)
+print("Fim Listagem")
+#421 343 543 76 675
--- Lista 3/1847.py
+++ Lista 3/1847.py
@@ -2,6 +2,12 @@ def felizes(): print(": )")
 def tristes(): print(": (")
 
 entradas = list(map(int, input().split()))
+
+if not any((i >= -100) for i in entradas) and not any((i <= 100) for i in entradas):
+    pass
+else:
+    print("????")
+
 subiuNoSegundo, subiuNoSTerceiro, subiuMaisNoSegundo, cresceuIgual, constanteSegundo, constanteTerceiro, cresceuMais =  False, False, False, False, False, False, False
 
 if entradas[0] < entradas[1]: subiuNoSegundo = True
