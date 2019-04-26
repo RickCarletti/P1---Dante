ef62e4c5e5e2f0741577dc6fc98fcd1e6d94b008
renatocarusouff <49883782+renatocarusouff@users.noreply.github.com>
2019-04-25 23:50:46 -0300

    .

---
+++ Lista 3/1037.py
@@ -0,0 +1,13 @@
+listaDeEntrada = "[0,25], (25,50], (50,75], (75,100]"
+valores = []
+indexC = 0
+def achaValor():
+        
+        for c in listaDeEntrada:
+        indexC += 1
+        if c == "[" or c == "(":
+            while c
+                    valores.append(listaDeEntrada[i])
+                else: pass
+
+print(valores)
--- Lista 3/1847.py
+++ Lista 3/1847.py
@@ -1,34 +1,35 @@
-def felizes(): print(": )")
-def tristes(): print(": (")
+def felizes(): print(":)")
+def tristes(): print(":(")
 
-entradas = list(map(int, input().split()))
+listaDeEntradas = list(map(int, input().split()))
 
-if not any((i >= -100) for i in entradas) and not any((i <= 100) for i in entradas):
-    pass
-else:
-    print("????")
+def estaoFelizesComEssasTemperaturas(entradas):
+    if any(i < -100 for i in entradas) or any(i > 100 for i in entradas):
+            return None
+    else:
+        subiuNoSegundo, subiuNoSTerceiro, subiuMaisNoSegundo, cresceuIgual, constanteSegundo, constanteTerceiro, cresceuMais =  False, False, False, False, False, False, False
 
-subiuNoSegundo, subiuNoSTerceiro, subiuMaisNoSegundo, cresceuIgual, constanteSegundo, constanteTerceiro, cresceuMais =  False, False, False, False, False, False, False
+        if entradas[0] < entradas[1]: subiuNoSegundo = True
+        if entradas[1] < entradas[2]: subiuNoSTerceiro = True
+        if (entradas[0] - entradas[1]) < (entradas[1] - entradas[2]) : cresceuMais = True
+        if (entradas[0] - entradas[1]) == (entradas[1] - entradas[2]) : cresceuIgual = True
+        if entradas[0] == entradas[1]: constanteSegundo = True
+        if entradas[1] == entradas[2]: constanteTerceiro = True
 
-if entradas[0] < entradas[1]: subiuNoSegundo = True
-if entradas[1] < entradas[2]: subiuNoSTerceiro = True
-if (entradas[0] - entradas[1]) < (entradas[1] - entradas[2]) : cresceuMais = True
-if (entradas[0] - entradas[1]) == (entradas[1] - entradas[2]) : cresceuIgual = True
-if entradas[0] == entradas[1]: constanteSegundo = True
-if entradas[1] == entradas[2]: constanteTerceiro = True
+        if constanteSegundo:
+            if subiuNoSTerceiro: felizes()
+            elif not subiuNoSTerceiro: tristes()
+        else:
+            if subiuNoSegundo:
+                if not subiuNoSTerceiro or constanteTerceiro: tristes()
+                elif subiuNoSTerceiro:
+                    if not cresceuMais: felizes()
 
-if constanteSegundo:
-    if subiuNoSTerceiro: felizes()
-    elif not subiuNoSTerceiro: tristes()
-else:
-    if subiuNoSegundo:
-        if not subiuNoSTerceiro or constanteTerceiro: tristes()
-        elif subiuNoSTerceiro:
-            if not cresceuMais: felizes()
+                    elif cresceuIgual or cresceuMais: tristes()
+            elif not subiuNoSegundo:
+                if subiuNoSTerceiro or constanteTerceiro: felizes()
+                elif not subiuNoSTerceiro:
+                    if cresceuMais or cresceuIgual: tristes()
+                    elif not cresceuMais or not cresceuIgual: felizes()
 
-            elif cresceuIgual or cresceuMais: tristes()
-    elif not subiuNoSegundo:
-        if subiuNoSTerceiro or constanteTerceiro: felizes()
-        elif not subiuNoSTerceiro:
-            if cresceuMais or cresceuIgual: tristes()
-            elif not cresceuMais or not cresceuIgual: felizes()
+estaoFelizesComEssasTemperaturas(listaDeEntradas)
