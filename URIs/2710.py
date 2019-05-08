763a14f335efbd70863cd15f926083e529c6c5d4
renatocarusouff <49883782+renatocarusouff@users.noreply.github.com>
2019-05-08 01:41:22 -0300

    .

---
+++ URIs/2710.py
@@ -0,0 +1,42 @@
+def ValidaMap(elemento):
+    if elemento.isdigit(): return int(elemento)
+    else: return elemento
+
+def ValidaEntrada(A, B, C, D, val):
+    listaDeValores = [A, B, C, D]
+    if all(i >= 1 and i <= 500 for i in listaDeValores) and abs(val) <= 5000:
+        return True
+
+def CriaMatriz(n_linhas, n_colunas, valor):
+    matriz = []
+    for i in range(n_linhas):
+        linha = []
+        for j in range(n_colunas):
+            linha.append(valor)
+        matriz.append(linha)
+
+    return matriz
+
+matriz = CriaMatriz(500,500, 0)
+saida = []
+operacoes = int(input())
+
+if operacoes <=100000:
+    for i in range(operacoes):
+        entrada = input().split()
+
+        if entrada[0] == 'U':
+            U, X1, Y1, X2, Y2, V = map(ValidaMap, entrada)
+            if ValidaEntrada(X1, Y1, X2, Y2, V):
+                for linha in range(10):
+                    if linha in range(X1, X2+1):
+                        for coluna in range(10):
+                            if coluna in range(Y1, Y2+1):
+                                matriz[linha][coluna] += V
+
+        if entrada[0] == 'A':
+            A, X, Y = map(ValidaMap, entrada)
+            saida.append(matriz[X][Y])
+
+for i in saida:
+    print(i)
