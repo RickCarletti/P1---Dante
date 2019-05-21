a2803f6b3bd18139bff29020a3276b8d9379bf2b
renatocarusouff <49883782+renatocarusouff@users.noreply.github.com>
2019-05-20 22:42:48 -0300

    .

---
+++ Aulas/DNA.py
@@ -0,0 +1,25 @@
+def geraDNA(tam):
+    from random import randint
+    frase = ""
+    baseNitrogenada = "CGAT"
+    for i in range(tam):
+        frase += baseNitrogenada[randint(0, len(baseNitrogenada)-1)]
+    return frase
+
+def examinaDNA(pDNA, dDNA, n):
+    if pDNA.count(dDNA) >= n:
+        return True
+    else:
+        return False
+
+tamDnaPessoa = int(input())
+tamDnaDoeca = int(input())
+qtdeOcorrencias = int(input())
+
+dnaPessoa = geraDNA(tamDnaPessoa)
+dnaDoenca = geraDNA(tamDnaDoeca)
+
+if examinaDNA(dnaPessoa, dnaDoenca, qtdeOcorrencias):
+    print("Doente")
+else:
+    print("Passou")
